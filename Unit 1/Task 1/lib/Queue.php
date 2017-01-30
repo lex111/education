1c157bd9319da1c9edc5346125514dea6feac492
Alexey Pyltsyn <lex61rus@gmail.com>
2017-01-30 07:27:50 +0300

    Unit 1. Task1. Реализация.

--- Unit 1/Task 1/lib/Queue.php
+++ Unit 1/Task 1/lib/Queue.php
@@ -6,5 +6,38 @@
 namespace WS\Education\Unit1\Task1;
 
 class Queue implements Collection {
+    /**
+     * @var array $collection
+     */
+    private $collection;
 
+    /**
+     * Queue constructor.
+     *
+     * @param string $type
+     */
+    public function __construct($type = 'queue') {
+        $this->collection = array();
+    }
+
+    /**
+     * @param $item
+     */
+    public function push($item) {
+        array_push($this->collection, $item);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function pop() {
+        return array_shift($this->collection);
+    }
+
+    /**
+     * @return int
+     */
+    public function size() {
+        return count($this->collection);
+    }
 }
--- Unit 1/Task 1/lib/Stack.php
+++ Unit 1/Task 1/lib/Stack.php
@@ -6,5 +6,42 @@
 namespace WS\Education\Unit1\Task1;
 
 class Stack implements Collection {
+    /**
+     * @var array $collection
+     */
+    private $collection;
 
+    /**
+     * Stack constructor.
+     *
+     * @param string $type
+     */
+    public function __construct($type = 'stack')
+    {
+        $this->collection = array();
+    }
+
+    /**
+     * @param $el
+     */
+    public function push($el)
+    {
+        array_push($this->collection, $el);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function pop()
+    {
+        return array_pop($this->collection);
+    }
+
+    /**
+     * @return int
+     */
+    public function size()
+    {
+        return count($this->collection);
+    }
 }
